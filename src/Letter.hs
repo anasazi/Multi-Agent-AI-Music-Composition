5f97cd0a1a2d15bb6132a276dfdb42b0ae49a529
Eric Reed <neuralnova@gmail.com>
2012-02-29 15:42:58 -0500

    added succ and pred letter specific functions

--- src/Letter.hs
+++ src/Letter.hs
@@ -1,16 +1,23 @@
-module NoteLetter
-( NoteLetter(..)
+module Letter
+( Letter(..)
+, succL, predL
 ) where
 
 import Test.QuickCheck
 import Data.Function (on)
 
-data NoteLetter = A | B | C | D | E | F | G
+data Letter = A | B | C | D | E | F | G
 	deriving (Eq, Show, Enum, Bounded)
 
-instance Ord NoteLetter where
+instance Ord Letter where
 	compare x y = (compare `on` length) (takeWhile (/=x) ls) (takeWhile (/=y) ls) 
 		where ls = [ C, D, E, F, G, A, B ]
 
-instance Arbitrary NoteLetter where
+instance Arbitrary Letter where
 	arbitrary = elements [ A, B, C, D, E, F, G ]
+
+succL B = C
+succL x = succ x
+
+predL C = B
+predL x = pred x
