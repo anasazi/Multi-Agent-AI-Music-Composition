e1e4693202038aa5672c6de836f8da2d3c3e207f
Eric Reed <neuralnova@gmail.com>
2012-03-19 03:41:53 -0400

    added a timeToTestAt field to the blackboard, a corresponding setter, and duration of CF and CP functions.

--- src/BlackboardSystem/Blackboard.hs
+++ src/BlackboardSystem/Blackboard.hs
@@ -1,9 +1,11 @@
 module BlackboardSystem.Blackboard
 ( Blackboard
 , cantusFirmus, scale
-, counterPoint
+, counterPoint, testResult, timeToTestAt
 , passTest, failTest
-, modifyCP
+, modifyCP, lookAt
+, create
+, durationOfCantusFirmus, durationOfCounterPoint
 ) where
 
 import Music.Voice
@@ -15,9 +17,22 @@ data Blackboard = Blackboard
                 , scale :: Scale -- the scale the piece is composed in
                 , counterPoint :: VoiceZipper -- the voice we're making
                 , testResult :: Bool -- did we pass this test?
+                , timeToTestAt :: Double -- where should we look?
                 }
+
+create cf s = Blackboard { cantusFirmus = cf
+                         , scale = s
+                         , counterPoint = enterFront []
+                         , testResult = False 
+                         , timeToTestAt = 0
+                         }
+
+lookAt bb t = bb { timeToTestAt = t }
 
 passTest bb = bb { testResult = True }
 failTest bb = bb { testResult = False }
 
 modifyCP bb f = bb { counterPoint = f (counterPoint bb) }
+
+durationOfCantusFirmus = durationOfVoice . cantusFirmus
+durationOfCounterPoint = durationOfVoice . counterPoint
