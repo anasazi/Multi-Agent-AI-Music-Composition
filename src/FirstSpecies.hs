85570619004354b0cba2e97afd4c3d417f3b7f2f
Eric Reed <neuralnova@gmail.com>
2012-03-26 03:34:03 -0400

    added consonant downbeats agent from first species

--- src/FirstSpecies.hs
+++ src/FirstSpecies.hs
@@ -13,7 +13,12 @@ import Music.Duration (whole)
 import System.Random
 
 agents :: [ KnowledgeSource ]
-agents = [ firstSpeciesGenerator ]
+agents = [ firstSpeciesGenerator 
+         , consonantDownbeats
+         ]
+
+safeHead (x:_) = Just x
+safeHead [] = Nothing
 
 {- Generate a whole note simultaneous with a note of the cantus firmus.
    Limit to one or zero accidentals. Limit to current note +/- 2 octaves.
@@ -30,7 +35,7 @@ firstSpeciesGenerator = makeGenerator (\bb ->
       (scaleIdx, gen') = randomR (0, lenBS scl - 1) gen 
       (octaveOff, gen'') = randomR (False, True) gen' 
       (octaveUp, gen''') = randomR (False, True) gen'' 
-      scalePitch = scl $# scaleIdx --getNote scl base scaleIdx
+      scalePitch = scl $# scaleIdx 
       offsetPitch | not octaveOff && octaveUp     = scalePitch
                   | octaveOff && octaveUp         = octave #^ scalePitch
                   | not octaveOff && not octaveUp = octave #. scalePitch
@@ -40,3 +45,12 @@ firstSpeciesGenerator = makeGenerator (\bb ->
       newBB = setGen (modifyCP bb (const newCP)) gen'''
   in if not isSimul then bb else newBB) 
   "First species generator"
+
+consonantDownbeats = makeHardRule (\bb ->
+  let cpNoteM = goToTime (counterPoint bb) (timeToTestAt bb) >>= safeHead . focus
+      cfNoteM = goToTime (cantusFirmus bb) (timeToTestAt bb) >>= safeHead . focus
+      intervalM = cpNoteM >>= \cp -> cfNoteM >>= \cf -> return (pitch cp # pitch cf)
+      isConsonantM = intervalM >>= \i -> let ls = lspan (simplify i) in return (ls `elem` [1,3,5,6,8])
+      result = maybe False id isConsonantM
+  in  (if result then passTest else failTest) bb)
+  "First species - all downbeats must be consonant (1st, 3rd, 5th, 6th, 8th & compounds)"
